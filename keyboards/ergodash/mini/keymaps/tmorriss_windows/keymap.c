82d077efeeab8a736f6fdcf2032610a19900f0d0
tMorriss <tmorriss@tmorriss.com>
2020-08-23 18:53:04 +0900

    fix windows PIPE

--- keyboards/ergodash/mini/keymaps/tmorriss_windows/keymap.c
+++ keyboards/ergodash/mini/keymaps/tmorriss_windows/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_LOWER] = LAYOUT( 
-    KC_TAB,  KC_EXLM, KC_HASH, KC_END,  KC_DLR,  KC_PERC, KC_GRV,                      KC_DEL,  KC_EQL,  KC_CIRC, KC_ASTR, KC_LPRN, KC_UP,   KC_PIPE, \
+    KC_TAB,  KC_EXLM, KC_HASH, KC_END,  KC_DLR,  KC_PERC, KC_GRV,                      KC_DEL,  KC_EQL,  KC_CIRC, KC_ASTR, KC_LPRN, KC_UP,   S(KC_JYEN), \
     KC_LCTL, KC_HOME, _______, KC_DEL,  _______, _______, KC_LBRC,                     KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_COLN, KC_PLUS, \
     KC_LSFT, _______, _______, _______, _______, _______, KC_LGUI,                     KC_SPC,  KC_DOWN, _______, KC_LT,   KC_GT,   _______, KC_GRV,  \
     KC_ESC,  KC_UNDS, KC_LALT, KC_LGUI,          KC_MHEN, LOWER, KC_SPC,        KC_ENT,RAISE,   KC_HENK,          KC_RGUI, _______, _______, _______  \
